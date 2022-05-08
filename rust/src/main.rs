9afbb304b25d21f189f0b4e6d41f9e53922e8867
Andreas Sahlbach <andreas@vw-dilab.com>
2022-05-08 11:56:55 +0200

    added optional keywords starting with # (note that # on a command line needs to be escaped to distinguish from comments)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -18,8 +18,13 @@ fn main() -> Result<(),Box<dyn Error>>{
     let matches = cmd.get_matches_mut();
     let json_file = matches.value_of("json");
     let brief = matches.is_present("brief");
-    let keywords: Vec<&str> = matches.values_of("KEYWORD").unwrap_or_default().collect();
-    let tags: Vec<&str> = matches.values_of("tag").unwrap_or_default().collect();
+    let mut keywords: Vec<&str> = matches.values_of("KEYWORD").unwrap_or_default().collect();
+    let mut tags: Vec<&str> = matches.values_of("tag").unwrap_or_default().collect();
+    println!("kw {:?} t {:?}",keywords,tags);
+    parse_tags_in_keywords(&mut keywords, &mut tags);
+    println!("kw {:?} t {:?}",keywords,tags);
+    let tags_case_sensitive = tags.iter().any(|t:&&str| (*t).to_lowercase() != *t);
+    let keywords_case_sensitive = keywords.iter().any(|k:&&str| (*k).to_lowercase() != *k);
 
     let reader: Box<dyn BufRead> = match json_file {
         None => Box::new(BufReader::new(io::stdin())),
@@ -49,7 +54,7 @@ fn main() -> Result<(),Box<dyn Error>>{
     let json = json_result.unwrap();
     if let Some(contents) = json["contents"].as_array() {
         let linkz = parser::parse_contents(contents);
-        let result = searcher::search_linkz(&linkz,&keywords,&tags);
+        let result = searcher::search_linkz(&linkz,&keywords,&tags,keywords_case_sensitive,tags_case_sensitive);
         for r in result.iter() {
             if brief {
                 println!("{}",r.url);
@@ -59,4 +64,20 @@ fn main() -> Result<(),Box<dyn Error>>{
         }
     }
     Ok(())
+}
+
+/// move all keywords starting with # into the tags vector
+fn parse_tags_in_keywords<'a>(keywords: &mut Vec<&'a str>, tags: &mut Vec<&'a str>) {
+    keywords.retain(|&k| {
+        match k.starts_with('#') {
+            true => {
+                let new_tag = &k[1..];
+                if !tags.contains(&new_tag) {
+                    tags.push(new_tag);
+                }
+                false
+            },
+            false => true
+        }
+    });
 }
--- rust/src/searcher.rs
+++ rust/src/searcher.rs
@@ -1,19 +1,32 @@
 use super::parser::Link;
 
-pub fn search_linkz(linkz : &Vec<Link>, keywords : &Vec<&str>, tags : &Vec<&str>) -> Vec<Link> {
+pub fn search_linkz(linkz : &Vec<Link>,
+                    keywords : &Vec<&str>,
+                    tags : &Vec<&str>,
+                    keywords_case : bool,
+                    tags_case : bool) ->
+                                                                                                           Vec<Link> {
     linkz.iter()
-        .filter(|link| link_has_all_keywords(*link, keywords))
-        .filter(|link| link_has_all_tags(*link, tags))
+        .filter(|link| link_has_all_keywords(*link, keywords, keywords_case))
+        .filter(|link| link_has_all_tags(*link, tags, tags_case))
         .cloned()
         .collect()
 }
 
-fn link_has_all_keywords(linkz : &Link,  keywords : &Vec<&str>) -> bool {
- keywords.iter().all(|i|linkz.url.contains(*i) || linkz.description.contains(*i))
+fn link_has_all_keywords(linkz : &Link,  keywords : &Vec<&str>, keywords_case: bool) -> bool {
+    match keywords_case {
+        true => keywords.iter().all(|i| { linkz.url.contains(*i) || linkz.description.contains(*i) }),
+        false => keywords.iter().all(|i| { linkz.url.to_lowercase().contains((*i).to_lowercase().as_str())
+            || linkz.description.to_lowercase().contains((*i).to_lowercase().as_str()) })
+    }
 }
 
-fn link_has_all_tags(linkz : &Link,  tags : &Vec<&str>) -> bool {
-    tags.iter().all(|i| linkz.tags.iter().any(|t| (*t).contains(*i)))
+fn link_has_all_tags(linkz : &Link,  tags : &Vec<&str>, tags_case: bool) -> bool {
+    match tags_case {
+        true => tags.iter().all(|i| linkz.tags.iter().any(|t| (*t).contains(*i))),
+        false => tags.iter().all(|i| linkz.tags.iter().any(|t|
+            (*t).to_lowercase().contains((*i).to_lowercase().as_str())))
+    }
 }
 
 #[cfg(test)]
@@ -21,17 +34,59 @@ mod test {
     use super::*;
 
     #[test]
+    fn search_case_sensitive_tags() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec![], &vec!["tagA"], false, true);
+        assert_eq!(res.len(),2);
+        let res = search_linkz(&linkz, &vec![], &vec!["taga"], false, false);
+        assert_eq!(res.len(),2);
+        let res = search_linkz(&linkz, &vec![], &vec!["Taga"], false, true);
+        assert_eq!(res.len(),0);
+    }
+
+    #[test]
+    fn search_case_sensitive_keywords() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec!["Descr"], &vec![], true, false);
+        assert_eq!(res.len(),2);
+        let res = search_linkz(&linkz, &vec!["descr"], &vec![], false, false);
+        assert_eq!(res.len(),2);
+        let res = search_linkz(&linkz, &vec!["descr"], &vec![], true, false);
+        assert_eq!(res.len(),0);
+    }
+
+    #[test]
     fn search_one_matching_tag() {
         let linkz = link_create();
-        let res = search_linkz(&linkz, &vec![], &vec!["tag1"]);
+        let res = search_linkz(&linkz, &vec![], &vec!["tag1"], false, false);
         assert_eq!(res.len(),1);
         assert_eq!(res[0].url, linkz[0].url);
+        let res = search_linkz(&linkz, &vec![], &vec!["tag3"], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[1].url);
+        let res = search_linkz(&linkz, &vec![], &vec!["tagA"], false, false);
+        assert_eq!(res.len(),2);
+        assert_eq!(res[0].url, linkz[0].url);
+        assert_eq!(res[1].url, linkz[1].url);
+    }
+
+    #[test]
+    fn search_two_matching_tags() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec![], &vec!["tag1","tag2"], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[0].url);
+        let res = search_linkz(&linkz, &vec![], &vec!["tag3","tag4"], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[1].url);
+        let res = search_linkz(&linkz, &vec![], &vec!["tag1","tag4"], false, false);
+        assert_eq!(res.len(),0);
     }
 
     #[test]
     fn search_one_multi_matching_tag() {
         let linkz = link_create();
-        let res = search_linkz(&linkz, &vec![], &vec!["tagA"]);
+        let res = search_linkz(&linkz, &vec![], &vec!["tagA"], false, false);
         assert_eq!(res.len(),2);
         assert_eq!(res[0].url, linkz[0].url);
         assert_eq!(res[1].url, linkz[1].url);
@@ -40,7 +95,50 @@ mod test {
     #[test]
     fn search_one_non_matching_tag() {
         let linkz = link_create();
-        let res = search_linkz(&linkz, &vec![], &vec!["tagX"]);
+        let res = search_linkz(&linkz, &vec![], &vec!["tagX"], false, false);
+        assert_eq!(res.len(),0);
+    }
+
+    #[test]
+    fn search_one_matching_keyword_and_tag() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec!["ABC"], &vec!["tag2"], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[0].url);
+        let res = search_linkz(&linkz, &vec!["DEF"], &vec!["tag2"], false, false);
+        assert_eq!(res.len(),0);
+        let res = search_linkz(&linkz, &vec!["XYZ"], &vec!["tagA"], false, false);
+        assert_eq!(res.len(),2);
+        assert_eq!(res[0].url, linkz[0].url);
+        assert_eq!(res[1].url, linkz[1].url);
+    }
+
+    #[test]
+    fn search_two_matching_keyword() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec!["ABC","GHI"], &vec![], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[0].url);
+        let res = search_linkz(&linkz, &vec!["DEF","JKL"], &vec![], false, false);
+        assert_eq!(res.len(),1);
+        assert_eq!(res[0].url, linkz[1].url);
+        let res = search_linkz(&linkz, &vec!["ABC","JKL"], &vec![], false, false);
+        assert_eq!(res.len(),0);
+    }
+
+    #[test]
+    fn search_one_multi_matching_keyword() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec!["XYZ"], &vec![], false, false);
+        assert_eq!(res.len(),2);
+        assert_eq!(res[0].url, linkz[0].url);
+        assert_eq!(res[1].url, linkz[1].url);
+    }
+
+    #[test]
+    fn search_one_non_matching_keyword() {
+        let linkz = link_create();
+        let res = search_linkz(&linkz, &vec!["notthere"], &vec![], false, false);
         assert_eq!(res.len(),0);
     }
 
@@ -48,12 +146,12 @@ mod test {
         vec![
             Link {
                 url: "http://123/".to_string(),
-                description: "Description ABC".to_string(),
+                description: "Description ABC GHIXYZ".to_string(),
                 tags: vec!["tag1".to_string(), "tag2".to_string(), "tagA".to_string()]
             },
             Link {
                 url: "http://456/".to_string(),
-                description: "Description DEF".to_string(),
+                description: "Description DEF XYZJKL".to_string(),
                 tags: vec!["tag3".to_string(), "tag4".to_string(), "tagA".to_string()]
             }]
     }
